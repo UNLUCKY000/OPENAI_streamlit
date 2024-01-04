d6bc4ff08d2b7faef545f5dace5f8c2a1051f8b3
UNLUCKY000 <62801726+UNLUCKY000@users.noreply.github.com>
2024-01-04 04:28:10 +0400

    Update streamlit_chatbot.py

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -13,14 +13,14 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.button("Generate"):
+if prompt := st.chat_input():
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
     openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": "Write a story"})
-    #st.chat_message("user").write(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
